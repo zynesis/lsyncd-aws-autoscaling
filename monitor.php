dd628761688a11ba87fe764a02c0e14c519d205a
U-Zyn Chua <chua@uzyn.com>
2013-02-16 23:58:27 +0800

    Obtaining instance info from load balancer

--- monitor.php
+++ monitor.php
@@ -8,3 +8,23 @@
 require 'config.php';
 require 'vendor/autoload.php';
 
+use Aws\ElasticLoadBalancing\ElasticLoadBalancingClient;
+
+$client = ElasticLoadBalancingClient::factory(array(
+    'key' => $AWS_CONF['key'],
+    'secret' => $AWS_CONF['secret'],
+    'region' => $AWS_CONF['region']
+));
+
+$balancers = $client->describeLoadBalancers(array(
+    'LoadBalancerNames' => array($AWS_CONF['load_balancer_name'])
+));
+
+if (empty($balancers)) {
+    trigger_error('Failed to obtain description of load balancer.', E_USER_ERROR);
+}
+if (empty($balancers['LoadBalancerDescriptions'][0]['Instances'])) {
+    trigger_error('No EC2 instances found.', E_USER_ERROR);
+}
+
+$instances = $balancers['LoadBalancerDescriptions'][0]['Instances'];
