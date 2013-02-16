b9f798cbfd2bfbddb7de9e34b5299c007380f5d8
U-Zyn Chua <chua@uzyn.com>
2013-02-16 22:41:30 +0800

    Target output

---
+++ lsynd.conf.lua
@@ -0,0 +1,15 @@
+-- Sample final lsyncd output
+
+settings = {
+    logfile = "/tmp/lsyncd.log",
+    statusFile = "/tmp/lsyncd.status",
+    nodaemon = true
+}
+
+sync {
+	default.rsync,
+	delay = 0,
+	source="/home/ubuntu/test", 
+	target="ubuntu@EC2-PRIVATE-IP:/home/ubuntu/test",
+	rsyncOps={"-e", "ssh -i /path/to/private-key.pem -o StrictHostKeyChecking=no"}
+}
