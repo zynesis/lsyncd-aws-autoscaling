6d8f383458f00e1d4b0284cf06a4d3011624f3cf
U-Zyn Chua <chua@uzyn.com>
2013-02-17 00:44:24 +0800

    Obtain private DNS names

--- monitor.php
+++ monitor.php
@@ -8,15 +8,17 @@
 require 'config.php';
 require 'vendor/autoload.php';
 
-use Aws\ElasticLoadBalancing\ElasticLoadBalancingClient;
+use Aws\Common\Aws;
 
-$client = ElasticLoadBalancingClient::factory(array(
+$aws = Aws::factory(array(
     'key' => $AWS_CONF['key'],
     'secret' => $AWS_CONF['secret'],
     'region' => $AWS_CONF['region']
 ));
 
-$balancers = $client->describeLoadBalancers(array(
+$elbClient = $aws->get('ElasticLoadBalancing');
+
+$balancers = $elbClient->describeLoadBalancers(array(
     'LoadBalancerNames' => array($AWS_CONF['load_balancer_name'])
 ));
 
@@ -27,14 +29,35 @@ if (empty($balancers['LoadBalancerDescriptions'][0]['Instances'])) {
     trigger_error('No EC2 instances found.', E_USER_ERROR);
 }
 
-$instances = $balancers['LoadBalancerDescriptions'][0]['Instances'];
+$elbEc2instances = $balancers['LoadBalancerDescriptions'][0]['Instances'];
 
 $slaves = array();
-foreach ($instances as $instance) {
+foreach ($elbEc2instances as $instance) {
     $instanceID = $instance['InstanceId'];
     if ($instanceID != $AWS_CONF['master_ec2_instance_id']) {
         $slaves[] = $instanceID;
     }
 }
 
-print_r($slaves);
+if (empty($slaves)) {
+    trigger_error('No slave instances found.', E_USER_ERROR);
+}
+
+$ec2Client = $aws->get('Ec2');
+
+$ec2Instances = $ec2Client->describeInstances(array('InstanceIds' => $slaves));
+//print_r($slaves);
+
+if (empty($ec2Instances)) {
+    trigger_error('Unable to obtain description of slave EC2 instances.', E_USER_ERROR);
+}
+
+$privateDNSNames = array();
+
+foreach ($ec2Instances['Reservations'] as $reservation) {
+    $instances = $reservation['Instances'];
+    
+    foreach ($instances as $instance) {
+        $privateDNSNames[$instance['InstanceId']] = $instance['PrivateDnsName'];
+    }
+}
