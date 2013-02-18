f6e3042b5f6f8968c1b98e470d0806054ba6adb1
U-Zyn Chua <chua@uzyn.com>
2013-02-18 21:12:59 +0800

    Added The MIT License

--- monitor.php
+++ monitor.php
@@ -1,9 +1,19 @@
 #!/usr/bin/env php
 <?php
 /**
- * 1. Monitors auto scaled instances in a group
- * 2. Generates lsyncd.conf.lua.
- * 3. Restart existing lsyncd
+ * lsyncd-aws-autoscaling
+ *
+ * Lsyncd auto configuration that works with Amazon Web Services (AWS) Auto Scaling
+ *
+ * It does the following:
+ * 1. Monitors auto scaled instances that are attached to a load balancer.
+ * 2. Generates lsyncd.conf.lua, if there are changes in the attached instances.
+ * 3. Monitors Lsyncd and make sure Lsyncd is always up and running
+ *
+ * @author       U-Zyn Chua <uzyn@zynesis.com>
+ * @copyright    Copyright © 2013 U-Zyn Chua & Zynesis Pte. Ltd.
+ * @link         http://zynesis.com
+ * @license      MIT License
  */
 require 'config.php';
 require 'utilities.php';
